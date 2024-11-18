345a14c79600e596fe880f8c92f4907d173f1b3b
Khosbayar707 <torutora0415@gmail.com>
2024-11-18 18:01:19 +0800

    snake

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 const config = { size: 20, width: 30, height: 30 };
+const boardEl = documnet.getElementById("board");
+boardEl.style.width = config.width * config.size + "px";
+boardEl.style.height = config.height * config.size + "px";
