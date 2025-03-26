c3cb97fd48fbbbb50ae6e471faa422b7733f8030
Khosbayar707 <torutora0415@gmail.com>
2025-03-26 22:07:43 +0800

    snake

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-// Хэмжээ өгөх
 let headY = 5;
 let headX = 4;
-
 let foodY;
 let foodX;
-
 let direction = "right";
 let nextDirection = direction;
-
 let tails = [
   { x: 2, y: 5 },
   { x: 3, y: 5 },
@@ -20,46 +16,37 @@ const boardEl = document.getElementById("board");
 boardEl.style.width = config.width * config.size + "px";
 boardEl.style.height = config.height * config.size + "px";
 
-// Дээшээ явуулах
 function goUp() {
   headY = headY - 1;
   if (headY < 0) {
-    alert("You dead");
+    alert("You are dead");
     restartGame();
   }
 }
-
-// Баруун тийш явуулах
 
 function goRight() {
   headX = headX + 1;
   if (headX === config.width) {
-    alert("You dead");
+    alert("You are dead");
     restartGame();
   }
 }
-
-// Доошоо явуулах
 
 function goDown() {
   headY = headY + 1;
   if (headY === config.height) {
-    alert("You dead");
+    alert("You are dead");
     restartGame();
   }
 }
-
-// Зүүн тийш явуулах
 
 function goLeft() {
   headX = headX - 1;
   if (headX < 0) {
-    alert("You dead");
+    alert("You are dead");
     restartGame();
   }
 }
-
-// Чиглэлээ өөрлөх товч ажлуулах
 
 function changeDirection(newDirection) {
   if (nextDirection === "up" || nextDirection === "down") {
@@ -72,9 +59,11 @@ function changeDirection(newDirection) {
     }
   }
 }
-// Хөдөлгөх
 
 function gameLoop() {
+  const oldHeadX = headX;
+  const oldHeadY = headY;
+
   switch (nextDirection) {
     case "up":
       goUp();
@@ -90,17 +79,17 @@ function gameLoop() {
       break;
   }
 
-  tails.push({ x: headX, y: headY });
-  tails.shift();
-  for (let i = 0; i < tails.length - 1; i++) {
+  for (let i = 0; i < tails.length; i++) {
     if (headX === tails[i].x && headY === tails[i].y) {
       alert("Game over");
       restartGame();
-    } else if (headX === tails[i].x && headY === tails[i].y) {
-      alert("Game over");
-      restartGame();
+      return;
     }
   }
+
+  tails.shift();
+  tails.push({ x: oldHeadX, y: oldHeadY });
+
   if (headX === foodX && headY === foodY) {
     tails.push({ x: headX, y: headY });
     generateFood();
@@ -108,6 +97,7 @@ function gameLoop() {
 
   render();
 }
+
 function listenKeys(event) {
   const key = event.code;
   switch (key) {
@@ -125,6 +115,7 @@ function listenKeys(event) {
       break;
   }
 }
+
 document.addEventListener("keydown", listenKeys);
 
 document.addEventListener("keydown", listenSpace);
@@ -141,7 +132,6 @@ function listenSpace(event) {
 function generateFood() {
   foodX = Math.floor(Math.random() * config.width);
   foodY = Math.floor(Math.random() * config.height);
-  return foodX, foodY;
 }
 
 function startGame() {
@@ -157,39 +147,32 @@ function pauseGame() {
   intervalValid = null;
 }
 
-function reset() {
-  (headY = 5), (headX = 5);
-}
-
 function restartGame() {
   headY = 5;
-  headX = 5;
+  headX = 4;
   tails = [
     { x: 2, y: 5 },
     { x: 3, y: 5 },
     { x: 4, y: 5 },
   ];
   nextDirection = "right";
+  clearInterval(intervalValid);
+  intervalValid = null;
   generateFood();
+  render();
 }
-function resumeGame() {}
 
-// Могой оруулах
 function render() {
-  let tailsHTML = "";
-  let foodHTML = "";
+  let snakeHTML = "";
   for (let i = 0; i < tails.length; i++) {
-    tailsHTML += `<div class="snake" style="width: ${
-      1 * config.size
-    }px; height: ${1 * config.size}px; top: ${
-      tails[i].y * config.size
-    }px; left: ${tails[i].x * config.size}px"></div>`;
-    foodHTML += `<div class="food" style="width: ${
-      1 * config.size
-    }px; height: ${1 * config.size}px; top: ${foodY * config.size}px; left: ${
-      foodX * config.size
+    snakeHTML += `<div class="snake" style="width: ${config.size}px; height: ${
+      config.size
+    }px; top: ${tails[i].y * config.size}px; left: ${
+      tails[i].x * config.size
     }px"></div>`;
   }
-  let snakeHTML = `${tailsHTML} ${foodHTML}`;
+  snakeHTML += `<div class="food" style="width: ${config.size}px; height: ${
+    config.size
+  }px; top: ${foodY * config.size}px; left: ${foodX * config.size}px"></div>`;
   boardEl.innerHTML = snakeHTML;
 }
